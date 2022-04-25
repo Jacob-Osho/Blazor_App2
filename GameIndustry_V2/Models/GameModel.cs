9d976da1821f74eb8d5c1e409b3f8d628c76b772
Jacob <akella2609@gmail.com>
2022-04-25 16:53:06 +0200

    started project

---
+++ GameIndustry_V2/Models/GameModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace GameIndustry_V2.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Image { get; set; }
+    }
+}
