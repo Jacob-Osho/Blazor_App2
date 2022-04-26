166573b7c6880cb1f50ebf738ffdf1ae32ae6496
Jacob <akella2609@gmail.com>
2022-04-26 13:37:56 +0200

    created genres editing method

--- GameIndustry_V2/Data/Repository/IRepository.cs
+++ GameIndustry_V2/Data/Repository/IRepository.cs
@@ -8,5 +8,8 @@ namespace GameIndustry_V2.Data.Repository
     public interface IRepository
     {
         List<GameModel> GetAllGames();
+        List<Genre> GetAllGenres();
+        Genre GetGenreById(int id);
+        bool  EditGenre(Genre editedGenre);
     }
 }
--- GameIndustry_V2/Data/Repository/MockGamesRepository.cs
+++ GameIndustry_V2/Data/Repository/MockGamesRepository.cs
@@ -1,12 +1,14 @@
 ﻿using GameIndustry_V2.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GameIndustry_V2.Data.Repository
 {
     public class MockGamesRepository : IRepository
     {
-        private List<GameModel> _gameModels;
+        readonly List<GameModel> _gameModels;
+        readonly List<Genre> _genres;
         public MockGamesRepository()
         {
             _gameModels = new List<GameModel>()
@@ -32,12 +34,56 @@ namespace GameIndustry_V2.Data.Repository
                     ReleaseDate = new DateTime(2020, 07, 28)
 
                 }
-
+            };
+            _genres = new List<Genre>()
+            {
+                new Genre()
+                {
+                    Id =0,
+                    Name = "Simulator"
+                },
+                new Genre()
+                {
+                    Id =1,
+                    Name = "RPG"
+                },new Genre()
+                {
+                    Id =2,
+                    Name = "Action"
+                },
+                 new Genre()
+                  {
+                      Id = 3,
+                      Name = "Horror"
+                  }
             };
         }
+    
         public List<GameModel> GetAllGames()
         {
             return _gameModels;
+        }
+
+        public List<Genre> GetAllGenres()
+        {
+            return _genres;
+        }
+        public Genre GetGenreById(int id)
+        {
+            return _genres.FirstOrDefault(x => x.Id.Equals(id));
+        }
+        public bool EditGenre(Genre editedGenre)
+        {
+            var oldGenre = _genres.FirstOrDefault(x=>x.Id.Equals(editedGenre.Id));
+            if(oldGenre is not null)
+            {
+                oldGenre = editedGenre;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
